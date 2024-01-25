bae968434dcc82dbcc53bd9fb3b28422a241e790
Okoniewitz <37485333+Okoniewitz@users.noreply.github.com>
2024-01-25 15:53:07 +0100

    Dodaj pliki projektów.

---
+++ Helpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BankAccount
+{
+    public static class Helpers
+    {
+        public static bool debug = true;
+        public static Player Player = Game.LocalPlayer;
+        public static void Cheats()
+        {
+            if (Game.isKeyPressed(Keys.L))
+            {
+               Game.DisplayText(GetNearestObject(0).Model.ToString(),5000);
+            }
+        }
+
+        public static GTA.Object GetNearestObject(Model model)
+        {
+            GTA.Object[] AllObjects;
+            if (model!=0)
+            {
+                AllObjects = World.GetAllObjects(model);
+            }
+            else AllObjects = World.GetAllObjects();
+
+            float Dist = 999999f;
+            GTA.Object FinalObject=null;
+            foreach(GTA.Object obj in AllObjects)
+            {
+                if(obj.Position.DistanceTo(Player.Character.Position)<Dist)
+                {
+                    Dist = obj.Position.DistanceTo(Player.Character.Position);
+                    FinalObject = obj;
+                }
+            }
+            return FinalObject;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+
+namespace BankAccount
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Tick += EachTick.OnTick;
+        }
+    }
+
+    public static class EachTick
+    {
+        public static Player Player = Game.LocalPlayer;
+        public static List<GTA.Object> ATMList = new List<GTA.Object>();
+        public static void OnTick(object sender, EventArgs e)
+        {
+            if (Helpers.debug) Helpers.Cheats();
+            GTA.Object[] ATMs = World.GetAllObjects(Model.FromString("0x6223E19A"));
+            foreach(GTA.Object ATM in ATMs) //mark atms on map
+            {
+                if(!ATMList.Contains(ATM))
+                {
+                    Blip blip = ATM.AttachBlip();
+                    blip.Color = BlipColor.Purple;
+                    blip.ShowOnlyWhenNear = true;
+                    ATMList.Add(ATM);
+                }
+            }
+            if(Player.Character.Position.DistanceTo(Helpers.GetNearestObject(Model.FromString("0x6223E19A")).Position)<=2)
+            {
+                Game.DisplayText("using atm");
+            }
+        }
+    }
+}
